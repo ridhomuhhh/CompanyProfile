f9a4374db4a1d7019dcf26d1048972d181d714e6
akmalzi <akmalzidanfah@gmail.com>
2025-12-18 14:47:13 +0700

    add admin management to filament

---
+++ profile-koperasi/app/Filament/Resources/Users/Schemas/UserForm.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Filament\Resources\Users\Schemas;
+
+use Filament\Forms\Components\DateTimePicker;
+use Filament\Forms\Components\TextInput;
+use Filament\Schemas\Schema;
+
+class UserForm
+{
+    public static function configure(Schema $schema): Schema
+    {
+        return $schema
+            ->components([
+                TextInput::make('name')
+                    ->label('Nama')
+                    ->required(),
+                TextInput::make('email')
+                    ->label('Alamat Email')
+                    ->email()
+                    ->required(),
+                TextInput::make('password')
+                    ->password()
+                    ->revealable()
+                    ->required(),
+            ]);
+    }
+}
